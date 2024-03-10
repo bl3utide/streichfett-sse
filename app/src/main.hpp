fe6d83a1dd7fb746da5aff325c1490c8085b4363
bl3utide <bl3utide@gmail.com>
2024-03-10 20:01:24 +0900

    v1.1.0

--- app/src/main.hpp
+++ app/src/main.hpp
@@ -1,7 +1,7 @@
 ﻿/*
     Streichfett SSE
     Copyright (C) 2024 bl3utide <bl3utide@gmail.com>
-    1.0.1
+    1.1.0
 */
 #pragma once
 
@@ -9,7 +9,7 @@ namespace StreichfettSse
 {
 
 #define DEF_APP_NAME        "StreichfettSse"
-#define DEF_APP_VERSION     "1.0"
+#define DEF_APP_VERSION     "1.1"
 #define DEF_APP_DEV_BY      "bl3utide"
 #define DEF_APP_DEV_YR      2024
 #define DEF_APP_TITLE       "Streichfett SSE"
